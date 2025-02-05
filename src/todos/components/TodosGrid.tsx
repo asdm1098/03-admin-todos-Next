c15ef13d28290574b8e8dce842eec03c84469a77
asdm1098 <asdm1098@gmail.com>
2025-02-05 17:01:20 +0100

    Actualizar TODO

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -1,5 +1,9 @@
-import { Todo } from "@prisma/client"
-import { TodoItem } from "./TodoItem"
+'use client';
+
+import { Todo } from "@prisma/client";
+import { TodoItem } from "./TodoItem";
+
+import * as api from '@/todos/helpers/todos';
 
 interface Props {
     todos?: Todo[]
@@ -11,7 +15,7 @@ export const TodosGrid = ({ todos = [] }: Props) => {
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
             {
                 todos.map( todo => (
-                    <TodoItem key={ todo.id } todo={ todo } />
+                    <TodoItem key={ todo.id } todo={ todo } toggleTodo={ api.updateTodo } />
                 ))
             }
         </div>
