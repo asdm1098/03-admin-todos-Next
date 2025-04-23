b191bbc8e7cfb0a1a003e3d7b814daa084379d34
asdm1098 <asdm1098@gmail.com>
2025-04-23 11:52:03 -0500

    Cookies - Client Side

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { setCookie } from "cookies-next";
 import { useState } from "react";
 
 // https://tailwindcomponents.com/component/radio-buttons-1
@@ -15,12 +16,15 @@ export const TabBar = ({ tabOptions = [1, 2, 3, 4], currentTab = 1}: Props) => {
 
     const [selected, setSelected] = useState(currentTab);
 
-    const onTabSelected = ( tab: number ) => { setSelected(tab); }
+    const onTabSelected = ( tab: number ) => { 
+        setSelected(tab);
+        setCookie( 'selectedTab', tab.toString() );
+    }
 
     return (
         <div className={
             `grid w-full space-x-2 rounded-xl bg-gray-200 p-2
-            ${ 'grid-cols-' + tabOptions.length.toString() }
+            ${ 'grid-cols-' + tabOptions.length }
             `}>
             {
                 tabOptions.map(tab => (
