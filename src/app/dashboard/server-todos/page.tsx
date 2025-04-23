b634dcc3618f1c1bf0d9646c988bd694bbda14e2
asdm1098 <asdm1098@gmail.com>
2025-04-23 04:23:05 -0500

    repaso

--- src/app/dashboard/server-todos/page.tsx
+++ src/app/dashboard/server-todos/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default async function ServerTodosPage() {
 
   const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' }});
-  
+
   return (
     <>
       <span className="text-3xl mb-10">Server Actions</span>
