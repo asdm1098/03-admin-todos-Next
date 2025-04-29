52c95cfa90e8983f8207726277b8c8172d0db65d
asdm1098 <asdm1098@gmail.com>
2025-04-29 12:43:54 -0500

    campos adicionales al usuario roles and id

--- auth.ts
+++ auth.ts
@@ -6,4 +6,36 @@ import Google from "next-auth/providers/google"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  session: {
+    strategy: "jwt"
+  },
+  callbacks: {
+    async signIn({ user }) {
+      console.log(user);
+      return true;
+    },
+
+    async jwt({ token, user }) {
+      // console.log({token});
+      const dbUser = await prisma.user.findUnique({
+        where: {
+          email: token.email ?? 'no-email',
+        },
+      })
+      token.roles = dbUser?.roles ?? ['no-roles'];
+      token.id = dbUser?.id ?? ['no-uuid'];
+
+      return token;
+    },
+
+    async session({ session, token, user }) {
+      console.log({session});
+      if ( session && session.user ) {
+        session.user.roles = token.roles;
+        session.user.id = token.id;
+      }
+      return session;
+    }
+  
+  }
 })
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,12 +8,18 @@ export default async function DashboardPage() {
   if (!session?.user) return redirect('/api/auth/signin');
 
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-6 md:grid-cols-1">
       <WidgetItem title="Usuario conectado S-Side">
         <div className="flex flex-col">
           <span> { session?.user?.name } </span>
           <span> { session?.user?.image } </span>
           <span> { session?.user?.email } </span>
+
+          <div>
+            {
+              JSON.stringify(session, null, 2)
+            }
+          </div>
         </div>
       </WidgetItem>
     </div>
