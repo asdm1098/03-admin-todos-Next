c473389ca904af2df861348c30382ba38b75325a
asdm1098 <asdm1098@gmail.com>
2025-04-29 12:03:01 -0500

    Prisma Adapter - AuthJS

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
+import prisma from "@/lib/prima"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
 })
